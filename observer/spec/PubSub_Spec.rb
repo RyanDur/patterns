f35937eea7d610e6eb9243521509ef078addd2a1
RyanDur <ryan.p.durling@gmail.com>
2013-08-08 16:51:50 -0700

    allow message expectations on nil

--- observer/spec/PubSub_Spec.rb
+++ observer/spec/PubSub_Spec.rb
@@ -6,6 +6,7 @@ describe 'PubSub' do
 
   before :each do
     @ps = PubSub.new
+    allow_message_expectations_on_nil
   end
 
   describe 'subscribe' do
@@ -56,14 +57,39 @@ describe 'PubSub' do
   end
 
   describe 'publish' do
-    it 'should publish an event if called' do
+    it 'should publish the event called' do
       @ps.subscribe 'button2Click' do
         @mock.qwer
       end
 
-      expect(@mock).to receive :qwer
+      @ps.subscribe 'button1Click' do
+        @mock.asdf
+      end
+
+      @mock.should_receive(:qwer)
+      expect(@mock).not_to receive :asdf
 
       @ps.publish('button2Click')
+    end
+
+    it 'should publish all the blocks for a certain event' do
+      @ps.subscribe 'button2Click' do
+        @mock.qwer
+      end
+
+      @ps.subscribe 'button2Click' do
+        @mock.qwer
+      end
+
+      @ps.subscribe 'button1Click' do
+        @mock.asdf
+      end
+
+      @mock.should_receive(:qwer).twice
+      expect(@mock).not_to receive :asdf
+
+      @ps.publish('button2Click')
+
     end
   end
 end
